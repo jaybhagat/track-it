d66924564135f77b4cc11dbdaa8cf753183afebf
Devraj Bhati <dbhati@uwaterloo.ca>
2022-10-18 20:48:39 -0400

    Delete user.kt

--- source/ToDo/src/main/kotlin/com/example/user.kt
+++
@@ -1,8 +0,0 @@
-@PostMapping("/api/add/user")
-
-
-public class User (private val id: Int, private val name: String, private val email: String, private val password: String) {
-    init {
-        println("Creating user: $email")
-    }   
-}
