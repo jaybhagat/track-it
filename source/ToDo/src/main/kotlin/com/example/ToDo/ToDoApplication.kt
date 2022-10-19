c0dd451923eecd8888c90250f9d3fd03a1fa9f31
Devraj Bhati <dbhati@uwaterloo.ca>
2022-10-18 22:58:11 -0400

    Trying to create user using POST

--- source/ToDo/src/main/kotlin/com/example/ToDo/ToDoApplication.kt
+++ source/ToDo/src/main/kotlin/com/example/ToDo/ToDoApplication.kt
@@ -7,15 +7,32 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.http.ResponseEntity
 import org.springframework.beans.BeanUtils
+import org.springframework.beans.BeansException
+import org.springframework.web.bind.annotation.RequestBody
 import java.sql.Connection
 import java.sql.DriverManager
 import java.sql.SQLException
+import java.net.URI
+import java.net.URLEncoder
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+
+public class User() {
+	public var id = null
+	public var name = null
+	public var username = null
+	public var password = null
+	init {
+		println("Creating user for: $name")
+	}
+}
 
 @SpringBootApplication
 class ToDoApplication
 
 @RestController
-class TaskController {
+class TaskController() {
 	var conn: Connection? = null
 	@GetMapping("/")
 	fun connect(): String? {
@@ -83,9 +100,49 @@ class TaskController {
 		}
 		return "done"
 	}
+
+	@PostMapping("/api/add/user")
+	fun createUser(@RequestBody getUserDetails: User): Int {
+		val newUser = User()
+		try {
+			BeanUtils.copyProperties(getUserDetails, newUser)
+		} catch (e: BeansException) {
+			println(e.message)
+			return 0
+		}
+		val con = conn;
+		try {
+			if (con != null) {
+				val sql =
+					"insert into users(id, name, username, password) values (${newUser.id}, ${newUser.name}, ${newUser.username}, ${newUser.password})"
+				val query = con.createStatement()
+				val results = query.executeQuery(sql)
+				println("Fetched data:")
+				while (results.next()) {
+					println(results.getString(1))
+				}
+			}
+		} catch (ex: SQLException) {
+			println(ex.message)
+			return 0
+		}
+		return 1
+	}
 }
 
+fun String.utf8(): String = URLEncoder.encode(this, "UTF-8")
+fun formData(data: Map<String, String>): HttpRequest.BodyPublisher? {
+	// Build string out of data
+	val result = data.map { (key, value) -> "${(key.utf8())}" }.joinToString { "&" }
+	return HttpRequest.BodyPublishers.ofString(result)
+}
 fun main(args: Array<String>) {
+	val userValues = mapOf("id" to "1", "name" to "Sarvesh", "username" to "cartman", "password" to "southpark")
+	val client = HttpClient.newBuilder().build()
+
+	val request = HttpRequest.newBuilder()
+		.uri(URI.create("https://localhost:8080/api/add/user"))
+		.POST(formData(userValues))
 	runApplication<ToDoApplication>(*args)
 }
 
--- source/ToDo/src/main/kotlin/com/example/ToDo/user.kt
+++ source/ToDo/src/main/kotlin/com/example/ToDo/user.kt
@@ -1,40 +1,40 @@
+/*
+    public class User() {
+        public var id = null
+        public var name = null
+        public var username = null
+        public var password = null
 
-public class User () {
-    public var id = NULL
-    public var name = NULL
-    public var username = NULL
-    public var password = NULL
-    init {
-        println("Creating user: $email")
-    }   
-} 
-
-@RestController
-@PostMapping("/api/add/user")
-public fun createUser(@RequestBody User getUserDetails): Int {
-    User newUser = User()
-    try {
-        BeanUtils.copyProperties(getUserDetails, newUser)
-    } catch (e: BeansException) {
-        println(e.message)
-        return 0
-    }
-    val con = conn;
-    try {
-        if (con != null) {
-            val sql = "insert into users(id, name, username, password) values (${newUser.id}, ${newUser.name}, ${newUser.username}, ${newUser.password})"
-            val query = con.createStatement()
-            val results = query.executeQuery(sql)
-            println("Fetched data:")
-            while(results.next()){
-                println(results.getString(1))
-            }
+        init {
+            println("Creating user for: $name")
         }
-    } catch (ex: SQLException) {
-        println(ex.message)
-        return 0
     }
-    return 1
-}
 
-
+    @PostMapping("/api/add/user")
+    public fun createUser(@RequestBody User getUserDetails): Int {
+        User newUser = User ()
+        try {
+            BeanUtils.copyProperties(getUserDetails, newUser)
+        } catch (e: BeansException) {
+            println(e.message)
+            return 0
+        }
+        val con = conn;
+        try {
+            if (con != null) {
+                val sql =
+                    "insert into users(id, name, username, password) values (${newUser.id}, ${newUser.name}, ${newUser.username}, ${newUser.password})"
+                val query = con.createStatement()
+                val results = query.executeQuery(sql)
+                println("Fetched data:")
+                while (results.next()) {
+                    println(results.getString(1))
+                }
+            }
+        } catch (ex: SQLException) {
+            println(ex.message)
+            return 0
+        }
+        return 1
+    }
+*/
