421984394e6cd32afcbea6e9ca3ede09ecb084e4
Devraj Bhati <dbhati@uwaterloo.ca>
2022-10-18 21:32:15 -0400

    Added PostMapping for creating new user

--- source/ToDo/src/main/kotlin/com/example/ToDo/ToDoApplication.kt
+++ source/ToDo/src/main/kotlin/com/example/ToDo/ToDoApplication.kt
@@ -5,6 +5,8 @@ import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.ResponseEntity
+import org.springframework.beans.BeanUtils
 import java.sql.Connection
 import java.sql.DriverManager
 import java.sql.SQLException
--- source/ToDo/src/main/kotlin/com/example/ToDo/user.kt
+++ source/ToDo/src/main/kotlin/com/example/ToDo/user.kt
@@ -1,8 +1,40 @@
-@PostMapping("/api/add/user")
 
-
-public class User (private val id: Int, private val name: String, private val email: String, private val password: String) {
+public class User () {
+    public var id = NULL
+    public var name = NULL
+    public var username = NULL
+    public var password = NULL
     init {
         println("Creating user: $email")
     }   
+} 
+
+@RestController
+@PostMapping("/api/add/user")
+public fun createUser(@RequestBody User getUserDetails): Int {
+    User newUser = User()
+    try {
+        BeanUtils.copyProperties(getUserDetails, newUser)
+    } catch (e: BeansException) {
+        println(e.message)
+        return 0
+    }
+    val con = conn;
+    try {
+        if (con != null) {
+            val sql = "insert into users(id, name, username, password) values (${newUser.id}, ${newUser.name}, ${newUser.username}, ${newUser.password})"
+            val query = con.createStatement()
+            val results = query.executeQuery(sql)
+            println("Fetched data:")
+            while(results.next()){
+                println(results.getString(1))
+            }
+        }
+    } catch (ex: SQLException) {
+        println(ex.message)
+        return 0
+    }
+    return 1
 }
+
+
